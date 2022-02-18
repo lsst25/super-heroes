4019664e0679a82441cf05b21012473b3493b065
Tatar Yuriy <lsst25@gmail.com>
2022-02-18 12:51:24 +0200

    Battle service refactored

--- src/app/main/battle/battle.service.ts
+++ src/app/main/battle/battle.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Hero } from '../../models/hero.model';
-import {BehaviorSubject, delay, Observable, of} from 'rxjs';
+import { BehaviorSubject, delay, Observable, of } from 'rxjs';
 import { StorageService } from '../../shared/services/storage.service';
 import { Battle } from '../../models/battle.model';
-import {StateStoreService} from "../../shared/services/state-store.service";
+import { StateStoreService } from '../../shared/services/state-store.service';
 
 @Injectable({
   providedIn: 'root',
@@ -12,18 +12,38 @@ export class BattleService {
   battles: Battle[] = [];
   battlesSubject = new BehaviorSubject<Battle[]>([...this.battles]);
 
-  constructor(private storage: StorageService,
-              private state: StateStoreService) {}
+  constructor(
+    private storage: StorageService,
+    private state: StateStoreService
+  ) {}
 
   battle(ownHero: Hero, enemyHero: Hero): Observable<Hero> {
     let boost = 0;
 
-    boost = ownHero.powerstats.combat > enemyHero.powerstats.combat ? boost + 0.1 : boost - 0.1;
-    boost = ownHero.powerstats.strength > enemyHero.powerstats.strength ? boost + 0.09 : boost - 0.09;
-    boost = ownHero.powerstats.power > enemyHero.powerstats.power ? boost + 0.08 : boost - 0.08;
-    boost = ownHero.powerstats.speed > enemyHero.powerstats.speed ? boost + 0.08 : boost - 0.08;
-    boost = ownHero.powerstats.durability > enemyHero.powerstats.durability ? boost + 0.08 : boost - 0.08;
-    boost = ownHero.powerstats.intelligence > enemyHero.powerstats.intelligence ? boost + 0.008 : boost - 0.08;
+    const {
+      combat: ownCombat,
+      strength: ownStrength,
+      power: ownPower,
+      speed: ownSpeed,
+      durability: ownDurability,
+      intelligence: ownIntelligence,
+    } = ownHero.powerstats;
+
+    const {
+      combat: enemyCombat,
+      strength: enemyStrength,
+      power: enemyPower,
+      speed: enemySpeed,
+      durability: enemyDurability,
+      intelligence: enemyIntelligence,
+    } = enemyHero.powerstats;
+
+    boost = ownCombat > enemyCombat ? boost + 0.1 : boost - 0.1;
+    boost = ownStrength > enemyStrength ? boost + 0.09 : boost - 0.09;
+    boost = ownPower > enemyPower ? boost + 0.08 : boost - 0.08;
+    boost = ownSpeed > enemySpeed ? boost + 0.08 : boost - 0.08;
+    boost = ownDurability > enemyDurability ? boost + 0.08 : boost - 0.08;
+    boost = ownIntelligence > enemyIntelligence ? boost + 0.008 : boost - 0.08;
 
     const battleResult = Math.random() < 0.5 + boost;
     const winner = battleResult ? ownHero : enemyHero;
